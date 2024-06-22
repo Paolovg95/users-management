de5a74b8644fcea08ccf01c1f35d62f48fb770e6
paolovg95 <paolo9517@gmail.com>
2024-06-22 17:23:59 -0400

    JS file added to read licitaciones detail template

---
+++ static/licitacion_row.js
@@ -0,0 +1,16 @@
+const obj = JSON.parse(document.getElementById('hello-data').textContent);
+for (const [key, value] of Object.entries(obj)) {
+  let hiddenRow = document.getElementById(`${key}-row`)
+  for (const [ofertante, pricing] of Object.entries(value)) {
+    let price = document.getElementById(pricing)
+    if (price != null) {
+      price.addEventListener("click", function () {
+        if (hiddenRow.hidden) {
+          hiddenRow.hidden = false
+        } else {
+          hiddenRow.setAttribute("hidden", true)
+        }
+      });
+    }
+  }
+}
